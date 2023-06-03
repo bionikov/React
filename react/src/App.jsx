cfaf3e968df697b52bd21649795109d229b5476d
bionikov <kuba141@o2.pl>
2023-06-03 13:20:50 +0200

    kompletna przemiana

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,7 +8,17 @@ function App() {
 
   return (
     <>
-    
+        <div id="header" class="px-28 py-5">
+          <nav class="flex items-center">
+            <img src="images/ferrari.png" class="max-h-20 max-w-20 "></img>
+            <ul class="flex-1 text-center">
+              <li class="list-none inline-block px-4"><a href="index.html">Ferari Enzo</a></li>
+              <li class="list-none inline-block px-4"><a href="index2.html">Ferari California</a></li>
+              <li class="list-none inline-block px-4"><a href="index3.html">Ferari 458 Italia</a></li>
+            </ul>
+            <img src="images/ferrari.png" class="max-h-20 max-w-20 "></img>
+          </nav>
+        </div>
     </>
   )
 }
